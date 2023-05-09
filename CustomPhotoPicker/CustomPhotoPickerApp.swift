6ec81fb0187f5142b4eda336ba68afcf2b90f6ea
arjundoel <arjundoel@gmail.com>
2023-05-09 20:11:13 +0100

    Initial Commit

---
+++ CustomPhotoPicker/CustomPhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPhotoPickerApp.swift
+//  CustomPhotoPicker
+//
+//  Created by Arjun Doel on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomPhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
